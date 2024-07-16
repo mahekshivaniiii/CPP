400a1a5cc65546a3fc7dbfeb0b67dbeb887ec841
mahekshivaniiii <mahekshivani42@gmail.com>
2024-07-16 11:45:49 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int num1 = 3 ;
+    int num2 = 3 ;
+
+    cout << num1+num2 <<endl; //6
+    cout <<  num1-num2 <<endl; //0
+    cout << num1*num2 << endl; //9
+    cout << num1/num2 <<endl; //1
+    cout << num1%num2 <<endl; //0
+
+    return 0;
+}

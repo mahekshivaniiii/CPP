7ff240b75e5b2d3504a0b5bb0d046c8ecafb9c0f
mahekshivaniiii <mahekshivani42@gmail.com>
2024-07-17 00:03:24 +0530

    left and right shift

---
+++ bitwise.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    int num1 =5 ;
+    int num2 = 16 ;
+
+    cout << (num1 >> 2) <<endl; // right shift
+    cout << (num2 << 2) <<endl;  //left shift 
+
+    return 0 ;
+}

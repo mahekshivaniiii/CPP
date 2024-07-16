8eb55336899cf0316fd635c0d3f28f9d0aee4fd3
mahekshivaniiii <mahekshivani42@gmail.com>
2024-07-16 22:58:06 +0530

    assignment operators

--- operators.cpp
+++ operators.cpp
@@ -15,5 +15,12 @@ int main(){
     cout << (num1!=num2) <<endl; //false 0
     cout << (num1 <=num2) <<endl; //true 1
 
+
+    num1 += 1;
+    num2 -=3;
+    cout <<num1<<endl;
+    cout << num2 <<endl;
+
+
     return 0;
 }

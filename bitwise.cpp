18f0d05d9994583e7304d3057e08d888bb3c26fe
mahekshivaniiii <mahekshivani42@gmail.com>
2024-07-17 00:10:39 +0530

    bitwise

--- bitwise.cpp
+++ bitwise.cpp
@@ -7,6 +7,9 @@ int main() {
 
     cout << (num1 >> 2) <<endl; // right shift
     cout << (num2 << 2) <<endl;  //left shift 
-
+    cout << (num1&num2) <<endl; //0
+    cout << (num1|num2) <<endl; //21
+    cout << (num1 ^ num2) <<endl; //21
+    cout << (~num1) <<endl; //2
     return 0 ;
 }

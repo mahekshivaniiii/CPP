8f7c0b987bc430661c8bd1b546991022ce6da403
mahekshivaniiii <mahekshivani42@gmail.com>
2024-07-18 23:38:32 +0530

    misc operator

---
+++ miscallenous.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int num1 = 4;
+cout << sizeof(num1) <<endl;
+
+   char b = 'a';
+
+   bool flag;
+
+   num1==b?flag=true:flag=false;
+    cout<<flag<<endl;
+
+
+    cout << (&b) <<endl;
+    return 0;
+}

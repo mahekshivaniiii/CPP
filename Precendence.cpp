8c64d110945680f74f1a47d87be9a2243d2cf30f
mahekshivaniiii <mahekshivani42@gmail.com>
2024-07-19 23:19:24 +0530

    Commit

---
+++ Precendence.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+
+    bool p = false;
+    bool q = false;
+    bool r = true;
+    cout << (p=q=r) ;
+    return 0 ;
+}
--- unary.cpp
+++ unary.cpp
@@ -4,6 +4,7 @@ int  main(){
 
     int num1 = (4,5,6) ;
     cout << (++num1) <<endl;
-    cout << num1 <<endl;
+    cout << (num1--) <<endl;
+    cout << num1 << endl;
     return 0 ;
 }
